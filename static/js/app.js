f29612ef2e8d87494b88a155c826d51f9612d899
Scott Cerny <scottcerny@yahoo.com>
2021-07-07 16:04:14 -0700

    updated index.html and created app.js

---
+++ static/js/app.js
@@ -0,0 +1,18 @@
+function init() {
+    d3.json("data/samples.json").then((data) => {
+      samples = data.names;
+      samples.map(samples => +samples)
+      var dropDown = d3.select("#selDataset");
+  
+      samples.forEach((sample) => {
+        dropDown
+        .append("option")
+        .attr("key", sample)
+        .text(sample)
+      });
+  
+      var sampleData = samples[0];
+      makeChart(sampleData);
+      dataPull(sampleData);
+    });
+  };
