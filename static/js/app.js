c541ff40266713c12a2fa668e4ea0321f569f29c
Scott Cerny <scottcerny@yahoo.com>
2021-07-08 15:19:15 -0700

    updated optionChanged function

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,23 @@
 function init() {
-    d3.json("data/samples.json").then((data) => {
-      samples = data.names;
-      samples.map(samples => +samples)
-      var dropDown = d3.select("#selDataset");
-  
-      samples.forEach((sample) => {
-        dropDown
-        .append("option")
-        .attr("key", sample)
-        .text(sample)
-      });
-  
-      var sampleData = samples[0];
-      makeChart(sampleData);
-      dataPull(sampleData);
+  d3.json("data/samples.json").then((data) => {
+    samples = data.names;
+    samples.map(samples => +samples)
+    var dropDown = d3.select("#selDataset");
+
+    samples.forEach((sample) => {
+      dropDown
+      .append("option")
+      .attr("key", sample)
+      .text(sample)
     });
-  };
+
+    var sampleData = samples[0];
+    makeChart(sampleData);
+    dataPull(sampleData);
+  });
+};
+
+function optionChanged(newData) {
+  dataPull(newData);
+  makeChart(newData);
+}
